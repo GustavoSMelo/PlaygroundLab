5833c43ed2852aaa98a4237641640a0eaa4ebab9
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-05-08 17:55:25 -0300

    scripts to apply force (jump) in unity

---
+++ ApplyForceUnity/ApplyForce.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplyForce : MonoBehaviour
+{
+
+    public float Force = 2.5f;
+    public Rigidbody2D ball;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.gameObject.CompareTag("Ball"))
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                ball.AddForce(new Vector2(0, Force * Time.deltaTime), ForceMode2D.Impulse);
+            }
+        }
+    }
+}
---
+++ ApplyForceUnity/ApplyForce2.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplyForce2 : MonoBehaviour
+{
+
+    public float vel = 350f;
+    public Rigidbody2D obj;
+    public bool CanJumpAgain = false;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (CanJumpAgain && Input.GetKeyDown(KeyCode.Space))
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                obj.AddForce(new Vector2(0, vel * Time.deltaTime), ForceMode2D.Impulse);
+
+            }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D anotherObj)
+    {
+        if (anotherObj.gameObject.CompareTag("ground"))
+        {
+            CanJumpAgain = true;
+        }
+
+    }
+
+    void OnCollisionExit2D(Collision2D anotherObj)
+    {
+        CanJumpAgain = false;
+    }
+}
