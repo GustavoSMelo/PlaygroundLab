6cd6c59e76aba3770f500f002c6a1d7834bbe502
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-07-31 02:01:53 -0300

    add style in page with styled component

--- testNext/pages/index.tsx
+++ testNext/pages/index.tsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React from 'react';
 import Link from 'next/link'
 import Head from 'next/head'
+import { Container } from '../styles/homeStyle'
 
 const Home = () => (
-	<>
+	<Container>
 		<Head>
-			<title>Home -  Hello World</title>
+			<title>Home</title>
 		</Head>
-		<h1>Hello world </h1>
-		<Link href='/users'><a>Users</a></Link>
-	</>
+		<h1>Hello World</h1>
+		<img src='/static/baixados.jpeg' width='200' />
+		<Link href='/users'><a>View users</a></Link>
+	</Container>
 )
 
 export default Home
