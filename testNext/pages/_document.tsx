d3305b4a31e68efaae70ee50fe548a2e806dac2a
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-07-31 02:07:14 -0300

    removing desnecessaries configurations

--- testNext/pages/_document.tsx
+++ testNext/pages/_document.tsx
@@ -2,21 +2,18 @@ import Document, { Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
 class MyDocument extends Document {
-	static getInitialProps({ renderPage }) {
+	/*static getInitialProps({ renderPage }) {
 		const sheet = new ServerStyleSheet();
 		const page = renderPage(App => props => sheet.collectStyles(<App {...props} />))
 
 		const styleTags = sheet.getStyleElement()
 
 		return { ...page, styleTags }
-	}
+	}*/
 
 	render() {
 		return (
 			<html>
-				<Head>
-					{this.props.styleTags}
-				</Head>
 				<body>
 					<Main />
 					<NextScript />
