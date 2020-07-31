60d270c1786f082f10bf6e1748e7e0cb91cbc6b1
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-07-31 02:01:17 -0300

    add configurations of styled components in DOM

--- testNext/pages/_document.tsx
+++ testNext/pages/_document.tsx
@@ -1,11 +1,21 @@
 import Document, { Head, Main, NextScript } from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
 
 class MyDocument extends Document {
+	static getInitialProps({ renderPage }) {
+		const sheet = new ServerStyleSheet();
+		const page = renderPage(App => props => sheet.collectStyles(<App {...props} />))
+
+		const styleTags = sheet.getStyleElement()
+
+		return { ...page, styleTags }
+	}
+
 	render() {
 		return (
 			<html>
 				<Head>
-					<style>{`body {background-color: #7159ac;}`}</style>
+					{this.props.styleTags}
 				</Head>
 				<body>
 					<Main />
