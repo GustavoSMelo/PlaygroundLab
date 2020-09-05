792776e5560a957e36723b6f285148bc37d2dc20
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-09-04 23:13:33 -0300

    first class of c#

---
+++ Class04Cs01/Carro.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class04Cs01
+{
+    public class Carro
+    {
+        public string modelo;
+        public string cor;
+        public float velMax;
+        public float velAtual;
+        public int qntdGas;
+
+        public void PrintAll()
+        {
+            Console.WriteLine("Modelo: " + modelo);
+            Console.WriteLine("Cor: " + cor);
+            Console.WriteLine("velocidade maxima: " + velMax + " km/h");
+            Console.WriteLine("velocidade Atual: " + velAtual + "km/h");
+            Console.WriteLine("Gasolina: " + qntdGas + " % ");
+        }
+    }
+}
---
+++ Class04Cs01/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Class04Cs01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro c = new Carro();
+            Carro c2 = new Carro();
+            Carro c3 = new Carro();
+
+            //Car 01:
+            c.cor = "vermelho";
+            c.modelo = "hb20";
+            c.velAtual = 120f;
+            c.velMax = 180f;
+            c.qntdGas = 52;
+
+            c.PrintAll();
+
+            System.Console.WriteLine("\n");
+            System.Console.WriteLine("==================");
+
+            //Car 02: 
+
+            c2.cor = "branco";
+            c2.modelo = "palio";
+            c2.velAtual = 60f;
+            c2.velMax = 120f;
+            c2.qntdGas = 76;
+
+            c2.PrintAll();
+            System.Console.WriteLine("\n");
+            System.Console.WriteLine("==================");
+
+            //Car 03:
+
+            c3.cor = "Prata";
+            c3.modelo = "S10";
+            c3.velAtual = 117.7f;
+            c3.velMax = 240f;
+            c3.qntdGas = 26;
+
+            c3.PrintAll();
+        }
+    }
+}
