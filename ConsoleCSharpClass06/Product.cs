b3153089ab7ef3d3e510a9ea5ad093c117626e3f
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-09-18 11:17:40 -0300

    exaple make in class

---
+++ ConsoleCSharpClass06/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleCSharpClass06
+{
+    public class Product
+    {
+        public string name;
+        public float price;
+        public int code;
+
+        public Product() { }
+
+        public void ShowDetails() {
+            System.Console.WriteLine("Name: " + this.name +
+                "\nPrice: " + this.price
+                +"\nCode: " + this.code);
+
+        }
+    }
+}
---
+++ ConsoleCSharpClass06/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleCSharpClass06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product p = new Product();
+            p.code = 1;
+            p.name = "Playstation 5";
+            p.price = 4999f;
+
+            p.ShowDetails();
+        }
+    }
+}
