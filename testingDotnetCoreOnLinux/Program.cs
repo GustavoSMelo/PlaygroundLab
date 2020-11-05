6edeb379921d6c7f0e332105f206b7f719bd6afc
Gustavo S. Melo <gsantos15569@gmail.com>
2020-11-05 02:55:39 -0300

    testing dotnet in linux (have a lot of troubles)

---
+++ testingDotnetCoreOnLinux/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using User;
+
+namespace testingDotnetCoreOnLinux
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User.UserModel user = new User.UserModel();
+            user.name = "Gustavo";
+            user.email = "gustavo@mail.com";
+            user.password = "gustavo";
+
+            Console.WriteLine(user.Scan());
+        }
+    }
+}
---
+++ testingDotnetCoreOnLinux/classes/User/UserModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace User
+{
+    public class UserModel
+    {
+
+    public String name;
+    public String email;
+    public String password;
+
+    public String Scan () {
+        return $"{name}, {email}, {password} ";
+    }
+    }
+}
