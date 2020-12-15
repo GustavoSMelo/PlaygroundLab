062b4d2a97ceccff47bef194b476f0468bb09dce
GustavoSMelo <gsantos15569@gmail.com>
2020-12-14 23:46:12 -0300

    update codes of toString

--- PHP/extends/index.php
+++ PHP/extends/index.php
@@ -1,10 +1,28 @@
 <?php
 
-    use src\Address\Address;
+    use src\Address;
+    use src\Event;
+    use src\LiveEvent;
+    use src\PresentialEvent;
 
-    $address = new Address("rua das palmeiras", 230, "0808080-30");
+    require __DIR__.'/src/event.php';
+    require __DIR__.'/src/live_event.php';
+    require __DIR__.'/src/presential_event.php';
+    require __DIR__.'/src/address.php';
+
+    $event = new Event("show de musica 2", 0, "05/11/2001", ["geovana"], 10);
+
+    $address = new Address("rua dos carros", 321, "132123");
+
     $presentialEvent = new PresentialEvent("show de musica", 1325.0, "11/11/2001", ["gustavo"], 68, $address);
-    $liveEvent = new LiveEvent("show de musica 2", 0, "05/11/2001", ["geovana"], 10, "youtube.com");
 
-    echo ($presentialEvent);
-    echo ($liveEvent);
+    $liveEvent = new LiveEvent("show de musica 2", 0, "05/11/2001",["geovana"], 10, "youtube.com");
+
+    $presentialData = $presentialEvent->__toString(); 
+    $liveData = $liveEvent->__toString(); 
+    
+    var_dump($presentialEvent->__toString());
+    echo '<br />';
+
+    var_dump($liveData);
+    echo '<br />';
