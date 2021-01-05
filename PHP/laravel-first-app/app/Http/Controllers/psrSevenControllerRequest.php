70d9744a32c95c67172ab9edc57b80abcfd34811
GustavoSMelo <gsantos15569@gmail.com>
2021-01-04 23:50:11 -0300

    trying import and use PSR-7 controller declaration

---
+++ PHP/laravel-first-app/app/Http/Controllers/psrSevenControllerRequest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Psr\Http\Message\ServerRequestInterface;
+
+class psrSevenControllerRequest extends Controller
+{
+    public function index (ServerRequestInterface $req) {
+    }
+}
