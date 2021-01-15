54be89fa1bf18b717d7133d7369e2cc668053cfc
GustavoSMelo <gsantos15569@gmail.com>
2021-01-15 19:38:08 -0300

    Add new relationships in model

--- PHP/laravel-with-database/app/Models/UserModel.php
+++ PHP/laravel-with-database/app/Models/UserModel.php
@@ -9,5 +9,20 @@ class UserModel extends Model
 {
     protected $table = 'user';
 
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'remember_token'
+    ];
+
+    public function sales () {
+        return $this->belongsTo(salesModel::class, 'id_product');
+    }
+
+    public function course () {
+        return $this->belongsToMany(coursesModel::class, 'user_courses', 'user_id', 'courses_id');
+    }
+
     use HasFactory;
 }
