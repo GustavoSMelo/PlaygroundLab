67888893446c1b25b8c9296beed5297b41618b3a
GustavoSMelo <gsantos15569@gmail.com>
2021-01-15 19:37:31 -0300

    new methods about sales controller

--- PHP/laravel-with-database/app/Http/Controllers/salesController.php
+++ PHP/laravel-with-database/app/Http/Controllers/salesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\salesModel;
 use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -10,8 +11,8 @@ class salesController extends Controller
 {
     public function index () {
         $sales = DB::table('sales')
-            ->join('users', 'sales.id_user', '=', 'users.id')
-            ->join('products', 'sales.id_product', '=', 'products.id')
+            ->join('user', 'sales.id_user', '=', 'user.id')
+            ->join('product', 'sales.id_product', '=', 'product.id')
             ->select()
             ->get();
 
@@ -27,14 +28,15 @@ class salesController extends Controller
                 'id_product' => ['bail', 'required', 'numeric']
             ]);
 
-            if(DB::table('users')->where('id', '=', $request->input('id_user'))->exists()){
-                if(DB::table('products')->where('id', '=', $request->input('id_product'))->exists()) {
+            if(DB::table('user')->where('id', '=', $request->input('id_user'))->exists()){
+                if(DB::table('product')->where('id', '=', $request->input('id_product'))->exists()) {
                     DB::table('sales')->insert([
                         'id_user' => $request->input('id_user'),
-                        'id_product' => $request->input('id_product')
+                        'id_product' => $request->input('id_product'),
+                        'payment_method' => $request->input('payment_method')
                     ]);
 
-                    DB::table('products')->where('id', '=', $request->input('id_product'))->decrement('stock', 1);
+                    //DB::table('product')->where('id', '=', $request->input('id_product'))->decrement('stock', 1);
 
                     return response()->json([
                         'message' => 'Thanks to buy in our marketplace, please, return again as soon possible, we are waiting for you'
@@ -52,5 +54,13 @@ class salesController extends Controller
             ], 400);
         }
 
+    }
+
+    public function showWithProduct (int $id) {
+        return salesModel::find($id)->product;// <- this isn't a method
+    }
+
+    public function showWithUser (int $id) {
+        return salesModel::find($id)->user;// "->user" isn't a method
     }
 }
