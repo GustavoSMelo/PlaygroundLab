a73271b21d7d668601829a24d286ccc3b0584bf6
GustavoSMelo <gsantos15569@gmail.com>
2021-01-15 19:36:27 -0300

    remove errors about validation arguments

--- PHP/laravel-with-database/app/Exceptions/Handler.php
+++ PHP/laravel-with-database/app/Exceptions/Handler.php
@@ -2,6 +2,7 @@
 
 namespace App\Exceptions;
 
+use Dotenv\Exception\ValidationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
 
@@ -13,7 +14,7 @@ class Handler extends ExceptionHandler
      * @var array
      */
     protected $dontReport = [
-        //
+        ValidationException::class
     ];
 
     /**
