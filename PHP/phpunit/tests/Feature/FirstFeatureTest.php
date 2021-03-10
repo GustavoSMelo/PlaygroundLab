5be9201e07838694aafcf415b881cf1d8d3082b8
GustavoSMelo <gsantos15569@gmail.com>
2021-03-10 02:14:40 -0300

    testing created with success

--- PHP/phpunit/tests/Feature/FirstFeatureTest.php
+++ PHP/phpunit/tests/Feature/FirstFeatureTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Str;
 use Tests\TestCase;
 
 class FirstFeatureTest extends TestCase
@@ -14,11 +15,12 @@ class FirstFeatureTest extends TestCase
 
         Artisan::call('migrate:fresh');
 
-        $name = 'gustavo';
-        $email = 'gustavo';
-        $password = 'gustavo';
+        $name = Str::random(10);
+        $email = Str::random(10).'@email.com';
+        $password = Str::random(10);
 
-        $response = $this->post('/api/user', [
+        $response = $this->withHeader('Content-Type', 'application/json')
+        ->json('POST','/api/user', [
             'name' => $name,
             'email' => $email,
             'password' => $password
@@ -27,5 +29,17 @@ class FirstFeatureTest extends TestCase
         $response
             ->assertStatus(200)
             ->assertJson(['message' => true]);
+    }
+
+    /**
+    *   @depends test_register_user
+    */
+    public function test_get_all_users () {
+        $response = $this->getJson('/api/user');
+
+        $this->assertIsObject($response);
+        $this->assertNotEmpty($response);
+
+        $response->assertStatus(200);
     }
 }
